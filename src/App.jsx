1f80449a90edb05bd81b4c58fdbbdc2d70c199e1
fatundem <fatundem@gmail.com>
2026-05-29 02:47:30 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ useEffect(() => {
   revenueActive: p.revenue_active,
   lastUpdate:    p.last_update,
   useCase:       p.use_case,
-}))))
+}))));
 
 
 const STAGE_IDX = Object.fromEntries(STAGES.map((s, i) => [s.id, i]));
