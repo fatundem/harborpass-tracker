9a11aa27fde1ee9a3bf9756c88745c4608a5f0f5
fatundem <fatundem@gmail.com>
2026-06-10 00:15:28 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // ── CONFIGURATION ─────────────────────────────────────────────────────────────
 export const SUPABASE_URL = "https://kkmgvmdjdijbovmhzwul.supabase.co";
 export const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtrbWd2bWRqZGlqYm92bWh6d3VsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAwMTczMjgsImV4cCI6MjA5NTU5MzMyOH0.smBFtagcKfd-8VRxv5CugcCpSRF8k-i1LgT7IjHUfEo";
-export const SHEETS_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ2vnKG-iTlRsMx9ecZZWBH8wjJ0TEXx2vFZlFs7-7-RV5rryKvmG5UObYa-GfxuQ/pubhtml?gid=872247566&single=true"; 
+export const SHEETS_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ2vnKG-iTlRsMx9ecZZWBH8wjJ0TEXx2vFZlFs7-7-RV5rryKvmG5UObYa-GfxuQ/pub?gid=872247566&single=true&output=csv"; 
 // ─────────────────────────────────────────────────────────────────────────────
 
 export function sbHeaders(token) {
